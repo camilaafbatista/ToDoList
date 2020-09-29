d1cb5fe932ab37ba5c1b8ac16e9eaad93b4b3d0b
camilabrantes <camila.abrantes.batista@usp.br>
2020-09-29 01:37:17 +0000

    finished project

--- program.py
+++ program.py
@@ -1,19 +1,23 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+import os
 
 db = SQLAlchemy()
-
+login = LoginManager()
+login.login_view = "routes.login"
 
 def create_app():
-    app = Flask(__name__, static_url_path="", static_folder="static")
-    app.config[
-        'SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://camidbusername:dbpasswords@db4free.net/camiflaskproject"
+	app = Flask(__name__, static_url_path="", static_folder="static")
+	app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://camidbusername:dbpasswords@db4free.net/camiflaskproject"
+	app.secret_key="A Very Secret Key That Nobody Will Guess"
 
-    db.init_app(app)
+	db.init_app(app)
+	login.init_app(app)
 
-    with app.app_context():
-        from mainroutes import routes
-        app.register_blueprint(routes)
+	with app.app_context():
+		from mainroutes import routes
+		app.register_blueprint(routes)
 
-        db.create_all()
-        return app
+		db.create_all()
+		return app
